5cfedb50f0685a2cb0374cef950bf60598a34adb
blakeparkinson <wizardplow@gmail.com>
2015-12-03 23:34:32 -0700

    2nd commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/client/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+
+
+
+module.exports = app;
